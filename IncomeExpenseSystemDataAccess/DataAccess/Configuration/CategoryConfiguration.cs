e68f32b2524c84c75afd7f5e1be5571ffe9e1588
CodeTime3 <var.codetime@gmail.com>
2026-02-20 20:43:29 +0100

    added the category config

---
+++ IncomeExpenseSystemDataAccess/DataAccess/Configuration/CategoryConfiguration.cs
@@ -0,0 +1,25 @@
+using IncomeExpenseSystemDataAccess.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IncomeExpenseSystemDataAccess.DataAccess.Configuration;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable("Categories");
+        
+        builder.HasKey(c => c.CategoryId);
+        
+        builder.Property(e => e.CategoryName)
+            .HasColumnType("varchar(50)")
+            .HasMaxLength(50)
+            .IsRequired();
+        
+        builder.HasMany(c => c.Transactions)
+            .WithOne(e => e.Category)
+            .HasForeignKey(e => e.CategoryId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
