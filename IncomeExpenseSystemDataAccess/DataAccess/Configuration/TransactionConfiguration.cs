3441987faca33afd7ec1b773a2648f1fc79067b1
CodeTime3 <var.codetime@gmail.com>
2026-02-20 20:44:23 +0100

    changed the configuration of the entity

--- IncomeExpenseSystemDataAccess/DataAccess/Configuration/TransactionConfiguration.cs
+++ IncomeExpenseSystemDataAccess/DataAccess/Configuration/TransactionConfiguration.cs
@@ -4,23 +4,37 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace IncomeExpenseSystemDataAccess.DataAccess.Configuration;
 
-public class ExpenseConfiguration : IEntityTypeConfiguration<Transaction>
+public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
 {
     public void Configure(EntityTypeBuilder<Transaction> builder)
     {
-        builder.ToTable("Expenses");
+        builder.ToTable("Transactions");
         
-        builder.Property(e => e.AmountExpense)
+        builder.HasKey(e => e.TransactionId);
+        
+        builder.Property(e => e.TransactionAmount)
             .HasColumnType("decimal(18,2)")
             .IsRequired();
         
-        builder.Property(e => e.DescriptionExpense)
+        builder.Property(e => e.TransactionDescription)
             .HasColumnType("varchar(200)")
             .HasMaxLength(200)
             .IsRequired();
         
-        builder.Property(e => e.DateExpense)
+        builder.Property(e => e.TransactionDate)
             .HasColumnType("datetime")
             .IsRequired();
+        
+        builder.Property(e => e.TransactionType)
+            .HasColumnType("int")
+            .IsRequired();
+
+        builder.HasOne(e => e.User)
+            .WithMany(e => e.Transactions)
+            .HasForeignKey(e => e.UserId);
+
+        builder.HasOne(e => e.Category)
+            .WithMany(e => e.Transactions)
+            .HasForeignKey(e => e.CategoryId);
     }
 }
