0ae08081599971784f7f9785664b48b9901d9b2a
CodeTime3 <var.codetime@gmail.com>
2026-02-24 20:35:42 +0100

    added a new migration with the relationships and a new entity

--- IncomeExpenseSystemDataAccess/Migrations/20260224192831_AddedFkUserIdOnCategoryAndAddedSystemCategory.Designer.cs
+++ IncomeExpenseSystemDataAccess/Migrations/20260224192831_AddedFkUserIdOnCategoryAndAddedSystemCategory.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using IncomeExpenseSystemDataAccess.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IncomeExpenseSystemDataAccess.Migrations
 {
     [DbContext(typeof(IncomeExpenseSystemDbContext))]
-    partial class IncomeExpenseSystemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260224192831_AddedFkUserIdOnCategoryAndAddedSystemCategory")]
+    partial class AddedFkUserIdOnCategoryAndAddedSystemCategory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ IncomeExpenseSystemDataAccess/Migrations/20260224192831_AddedFkUserIdOnCategoryAndAddedSystemCategory.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IncomeExpenseSystemDataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedFkUserIdOnCategoryAndAddedSystemCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "SystemCategoryId",
+                table: "Transactions",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Categories",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateTable(
+                name: "SystemCategories",
+                columns: table => new
+                {
+                    SystemCategoryId = table.Column<Guid>(type: "char(36)", nullable: false),
+                    SystemCategoryName = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SystemCategories", x => x.SystemCategoryId);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_SystemCategoryId",
+                table: "Transactions",
+                column: "SystemCategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Categories_UserId",
+                table: "Categories",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Users_UserId",
+                table: "Categories",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transactions_SystemCategories_SystemCategoryId",
+                table: "Transactions",
+                column: "SystemCategoryId",
+                principalTable: "SystemCategories",
+                principalColumn: "SystemCategoryId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Users_UserId",
+                table: "Categories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transactions_SystemCategories_SystemCategoryId",
+                table: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "SystemCategories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Transactions_SystemCategoryId",
+                table: "Transactions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Categories_UserId",
+                table: "Categories");
+
+            migrationBuilder.DropColumn(
+                name: "SystemCategoryId",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Categories");
+        }
+    }
+}
