097c24ec121033dfb7f70f9592d81ffefab13970
CodeTime3 <var.codetime@gmail.com>
2026-03-03 20:50:41 +0100

    added constructor

--- IncomeExpenseSystemDataAccess/Entities/SystemCategory.cs
+++ IncomeExpenseSystemDataAccess/Entities/SystemCategory.cs
@@ -5,4 +5,17 @@ public class SystemCategory
     public Guid SystemCategoryId { get; set; }
     public string SystemCategoryName { get; set; }
     public ICollection<Transaction> Transactions { get; set; }
+    
+    public SystemCategory() {}
+
+    public SystemCategory(string systemCategoryName)
+    {
+        SystemCategoryName = systemCategoryName;
+    }
+    
+    public SystemCategory(Guid systemCategoryId, string systemCategoryName)
+    {
+        SystemCategoryId = systemCategoryId;
+        SystemCategoryName = systemCategoryName;
+    }
 }
