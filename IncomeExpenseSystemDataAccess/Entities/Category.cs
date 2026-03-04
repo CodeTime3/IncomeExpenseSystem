45485f790688296293a87252e5aee2ba843a5b11
CodeTime3 <var.codetime@gmail.com>
2026-03-04 20:35:19 +0100

    added constructor

--- IncomeExpenseSystemDataAccess/Entities/Category.cs
+++ IncomeExpenseSystemDataAccess/Entities/Category.cs
@@ -7,4 +7,19 @@ public class Category
     public ICollection<Transaction> Transactions { get; set; }
     public Guid UserId { get; set; }
     public User User { get; set; }
+    
+    public Category() {}
+    
+    public Category(string categoryName, Guid userId)
+    {
+        CategoryName = categoryName;
+        UserId = userId;
+    }
+    
+    public Category(Guid categoryId, string categoryName, Guid userId)
+    {
+        CategoryId = categoryId;
+        CategoryName = categoryName;
+        UserId = userId;
+    }
 }
