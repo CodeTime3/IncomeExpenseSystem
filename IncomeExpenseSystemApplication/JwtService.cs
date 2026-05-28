3aa9db612cdbccb539dfbd0f5a617db6c9efcc74
CodeTime3 <var.codetime@gmail.com>
2026-05-28 20:47:34 +0200

    modified jwt service

--- IncomeExpenseSystemApplication/JwtService.cs
+++ IncomeExpenseSystemApplication/JwtService.cs
@@ -1,42 +1,27 @@
-﻿using Microsoft.IdentityModel.JsonWebTokens;
-using System.Security.Claims;
+﻿using System.Security.Claims;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
-using Microsoft.Extensions.Configuration;
 using IncomeExpenseSystemDomain.Models;
 
 namespace IncomeExpenseSystemApplication;
 
 public class JwtService
 {
-    private readonly IConfiguration _configuration;
-
-    public JwtService(IConfiguration configuration)
-    {
-        _configuration = configuration;
-    }
-
-    public string CreateJwt(UserModel user)
+    public string CreateJwt(JwtModel model, Guid userId)
     {
         var claims = new List<Claim>
         {
-            new (ClaimTypes.NameIdentifier, user.UserId.ToString())
+            new (ClaimTypes.NameIdentifier, userId.ToString())
         };
-
-        var token = _configuration.GetValue<string>("JwtSettings:Token");
-        if (string.IsNullOrEmpty(token))
-        {
-            return "";
-        }
         
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(token));
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(model.Token));
         var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
 
         var tokenDescriptor = new JwtSecurityToken
         (
-            issuer: _configuration.GetValue<string>("JwtSettings:Issuer"),
-            audience: _configuration.GetValue<string>("JwtSettings:Audience"),
+            issuer: model.Issuer,
+            audience: model.Audience,
             claims: claims,
             expires: DateTime.UtcNow.AddDays(7),
             signingCredentials: credentials
